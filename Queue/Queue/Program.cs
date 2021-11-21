28b529047278f12db3472716405997801428e13c
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-11-21 14:04:24 +0530

    Queue

---
+++ Queue/Queue/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Queue
+{
+   
+        class Program
+        {
+            static void Main(string[] args)
+            {
+            Queue<int> st = new Queue<int>();
+
+            st.Enqueue(56);
+                st.Enqueue(30);
+                st.Enqueue(70);
+                foreach (object  obj in st)
+                {
+                    Console.WriteLine(obj);
+                }
+            }
+        }
+    }
