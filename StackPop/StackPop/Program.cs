257388c0c887a8ba78b11fd5b00be5b73fb003b3
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-11-21 13:59:13 +0530

    Stack Pop

---
+++ StackPop/StackPop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackUC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> st = new Stack<int>();
+
+            st.Push(56);
+            st.Push(30);
+            st.Push(70);
+            
+            foreach (object obj in st)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("Data in the stack :" + st.Count);
+            Console.WriteLine("peek data in the stack :" + st.Peek());
+            Console.WriteLine("deleted data in the satck : " + st.Pop());
+            Console.ReadKey();
+        }
+        }
+    }
