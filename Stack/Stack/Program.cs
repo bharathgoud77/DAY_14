0918acd294aa15bef29e2dc9e126fdca2a91beb5
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-11-21 13:36:41 +0530

    Stack Push

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> st = new Stack<int>();
+
+            st.Push(56);
+            st.Push(30);
+            st.Push(70);
+            
+            foreach (Object obj in st)
+            {
+                Console.WriteLine(obj);
+            }
+
+        }
+    }
+}
