90f1e205a242a44079e48c937be43161767a1d43
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-11-22 18:13:33 +0530

    LinkedList

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> LinkedList = new List<int>();
+            LinkedList.Add(56);
+            LinkedList.Add(70);
+            LinkedList.Add(30);
+            foreach (object obj in LinkedList)
+            {
+                Console.WriteLine(obj);
+                Console.ReadKey();
+            }
+        }
+    }
+}
