db56c6fc7080d59549c3bd80a2f59a8aab5a8a66
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-11-22 21:42:43 +0530

    linkedlist

---
+++ 2dArray/2dArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArraayDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[5, 2] { { 0, 0 }, { 1, 1 }, { 2, 2 }, { 3, 3 }, { 4, 4 } };
+            int i, j;
+            for (i = 0; i < 5; i++)
+            {
+
+                for (j = 0; j < 2; j++)
+                {
+                    Console.Write(" {0} ", a[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ LinkedListUC_4/LinkedListUC_4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> LinkedList = new List<int>();
+            LinkedList.Add(56);
+            LinkedList.Add(70);
+            foreach (object obj in LinkedList)
+            {
+                Console.WriteLine(obj);
+            }
+            LinkedList.Insert(1, 30);
+            Console.WriteLine(" inserting 30 between 56 and 70");
+            foreach (object obj in LinkedList)
+            {
+                Console.WriteLine(obj);
+                //Console.ReadKey();
+            }
+
+
+
+        }
+    }
+}
