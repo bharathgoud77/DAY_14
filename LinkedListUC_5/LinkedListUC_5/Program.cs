1183044e40eb7de254e7215acd3f0b7f8af05195
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-11-22 21:49:48 +0530

    LinkedList

---
+++ LinkedListUC_5/LinkedListUC_5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListUC_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> LinkedList = new List<int>();
+            LinkedList.Add(56);
+            LinkedList.Add(30);
+            LinkedList.Add(70);
+            foreach(object obj in LinkedList)
+            {
+                Console.WriteLine(obj);
+
+            }
+            LinkedList.RemoveAt(0);
+            Console.WriteLine("after removing the element");
+            foreach(object obj in LinkedList)
+            {
+                Console.WriteLine(obj);
+
+            }
+
+        }
+    }
+}
