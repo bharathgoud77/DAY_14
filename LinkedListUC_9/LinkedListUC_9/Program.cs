20b338d509964f549262701a85e0e79dcc0fcdc3
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-11-22 22:13:23 +0530

    Linekd List

---
+++ LinkedListUC_9/LinkedListUC_9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> LinkedList = new List<int>();
+            LinkedList.Add(56);
+            LinkedList.Add(30);
+            LinkedList.Add(40);
+            LinkedList.Add(70);
+            foreach (object obj in LinkedList)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("after removing");
+            int size = LinkedList.Count;
+            for (int i = 0; i < size; i++)
+            {
+                if (LinkedList[i] == 40)
+                {
+                    LinkedList.RemoveAt(i);
+                    break;
+                }
+            }
+            Console.WriteLine("Size is " + LinkedList.Count);
+            foreach (object obj in LinkedList)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
