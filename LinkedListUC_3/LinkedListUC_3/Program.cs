0d68d7d315c7ba36e15344b47dba4afc29c5bf65
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-11-22 18:30:29 +0530

    LinkedList

---
+++ LinkedListUC_3/LinkedListUC_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListUC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> LinkedList = new List<int>();
+            LinkedList.Add(56);
+            LinkedList.Add(30);
+            LinkedList.Add(70);
+
+            foreach (object obj in LinkedList)
+            {
+                Console.WriteLine(obj);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
