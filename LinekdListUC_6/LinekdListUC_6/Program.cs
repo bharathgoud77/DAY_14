c32d63eba8d584c53f12f5dcee474e88ed3c750e
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-11-22 21:56:55 +0530

    LInked List

---
+++ LinekdListUC_6/LinekdListUC_6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace LinkedListUC_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> LinkedList = new List<int>();
+            LinkedList.Add(56);
+            LinkedList.Add(30);
+            LinkedList.Add(70);
+            foreach(object obj in LinkedList)
+            {
+                Console.WriteLine(obj);
+
+            }
+            int size = LinkedList.Count;
+            LinkedList.RemoveAt(2);
+            Console.WriteLine("after removing the last element");
+            foreach (object obj in LinkedList)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
